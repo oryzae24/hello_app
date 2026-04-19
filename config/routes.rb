37db529e66fe4fb0cfa0e466ff3f937e0f97e584
oryzae24 <134453358+oryzae24@users.noreply.github.com>
2026-04-19 15:32:17 +0000

    switch to goodbye

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@
 #   root "hello#index"
 # end
 
-Rails.application.routes.draw do
-  root "application#hello"
-end
-
 # Rails.application.routes.draw do
-#   root 'application#goodbye'
+#   root "application#hello"
 # end
+
+Rails.application.routes.draw do
+  root 'application#goodbye'
+end
